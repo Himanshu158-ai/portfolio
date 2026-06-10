07b24838400ad9597d647f8d71b67554cf1ba8c2
Himanshu Singh <hs5317610@gmail.com>
2026-06-10 11:48:51 +0530

    feat: implement landing page layout with responsive navigation and hero section

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,46 +1,31 @@
 import img from "../assets/a.jpeg"
 import { ArrowDownRight, ArrowUpRight, Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const landing = () => {
     const [isOpen, setIsOpen] = useState(false);
-    return (
-        // <div className='h-screen bg-[#E8E8E3] font-ppwatch'>
-        //     <nav className='h-15 flex justify-between items-center px-10 pt-10'>
-        //         <div className="text-[#6B645C] text-md font-medium tracking-wide">
-        //             Web Developer &amp; Designer
-        //         </div>
-        //         <div className="hidden md:flex space-x-4">
-        //             <a className="text-[#6B645C] text-md font-medium tracking-wide hover:text-custom-black transition-colors duration-300" href="#">Services</a>
-        //             <a className="text-[#6B645C] text-md font-medium tracking-wide hover:text-custom-black transition-colors duration-300" href="#">Works</a>
-        //             <a className="text-[#6B645C] text-md font-medium tracking-wide hover:text-custom-black transition-colors duration-300" href="#">About</a>
-        //             <a className="text-[#6B645C] text-md font-medium tracking-wide hover:text-custom-black transition-colors duration-300" href="#">Contact</a>
-        //         </div>
-        //     </nav>
-        //     <div className='h-45 flex justify-center items-center'>
-        //         <h3 className='text-[6rem] font-bold text-[#171717] tracking-tight'>HIMANSHU SINGH</h3>
-        //     </div>
-        //     <div className='h-[calc(100vh-15rem)] grid grid-cols-3'>
-        //         <div className='flex flex-col justify-around items-start px-10'>
-        //             <ArrowDownRight className="text-[#6B645C] text-md font-medium tracking-wide" size={40} />
-        //             <p className='w-[3/4] text-lg font-medium text-[#6B645C] tracking-wide'>I build fast, modern websites that help businesses grow, available for freelance projects worldwide.</p>
-        //             <button className='bg-[#393632] text-white px-8 py-2 text-lg font-bold rounded-full transition-all duration-300 hover:bg-[#8C8C73] hover:text-white'>Contact</button>
-        //         </div>
-        //         <div className='h-full flex justify-center items-center'>
-        //             <img src={img} className='h-[19rem] w-[14rem] object-cover rounded-lg' alt="image" />
-        //         </div>
-        //         <div className='flex items-end justify-end pb-10'>
-        //             <div className='flex flex-col items-end px-10 '>
-        //                 <p className='text-[#6B645C] text-md font-medium tracking-wide'>Available For</p>
-        //                 <h2 className='text-[#393632] text-6xl font-bold tracking-tight'>JUN'26</h2>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
 
+    const [availableDate, setAvailableDate] = useState("");
+
+    useEffect(() => {
+        const date = new Date();
+
+        const month = date
+            .toLocaleString("en-US", { month: "short" })
+            .toUpperCase();
+
+        const year = date
+            .getFullYear()
+            .toString()
+            .slice(-2);
+
+        setAvailableDate(`${month}'${year}`);
+    }, []);
+
+    return (
         <div className='min-h-screen bg-[#E8E8E3] font-ppwatch'>
-            <nav className='relative h-15 flex justify-between items-center px-5 md:px-10 pt-2 md:pt-4'>
-                <div className="text-[#6B645C] text-sm md:text-md font-medium tracking-wide">
+            <nav className='relative h-15 flex justify-between items-center px-5 md:px-10 pt-6 md:pt-4'>
+                <div className="text-[#6B645C] text-sm md:text-md font-medium tracking-wide w-[10rem] md:w-[20rem]">
                     Web Developer &amp; Designer
                 </div>
 
@@ -94,13 +79,13 @@ const landing = () => {
                 )}
             </nav>
 
-            <div className='flex justify-center items-center px-4 mt-[8rem] md:mt-0'>
+            <div className='flex flex-col justify-center items-center px-4 mt-[12rem] md:mt-0'>
                 <h3 className='text-[2.5rem] sm:text-[4rem] md:text-[6rem] font-bold text-[#171717] tracking-tight text-center leading-[2.1rem] md:leading-[1]'>
                     HIMANSHU SINGH
                 </h3>
             </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-0 pt-10 md:pt-20 md:h-[calc(100vh-15rem)]'>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-0 pt-10 md:mt-12 md:h-[calc(100vh-15rem)]'>
 
                 <div className='flex flex-col justify-around items-center md:items-start px-5 md:px-10 gap-6 md:gap-0 text-center md:text-left'>
                     <ArrowDownRight
@@ -132,7 +117,7 @@ const landing = () => {
                 <div className='flex justify-center items-center'>
                     <img
                         src="https://i.pinimg.com/1200x/e7/f2/f1/e7f2f17de35ce63c6621c957a3d8c7e8.jpg"
-                        className='h-[12rem] w-[8rem] md:h-[19rem] md:w-[13rem] object-cover rounded-lg'
+                        className='h-[12rem] w-[8rem] md:h-[16rem] md:w-[11rem] object-cover rounded-lg'
                         alt="image"
                     />
                 </div>
@@ -143,8 +128,8 @@ const landing = () => {
                             Available For Hire
                         </p>
 
-                        <h2 className='text-[#393632] text-4xl md:text-6xl font-bold tracking-tight'>
-                            JUN'26
+                        <h2 className='text-[#393632] text-4xl md:text-5xl font-bold tracking-tight'>
+                            {availableDate}
                         </h2>
                     </div>
                 </div>
